9033cb1907677b02c98618dc05f57d8a458ad889
sousben <jeremy.j.bensoussan@gmail.com>
2016-10-04 22:54:40 +0300

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^oauth', hello.views.oauth),
     url(r'^poll/(?P<pollName>\w+)/', hello.views.poll)
 ]
