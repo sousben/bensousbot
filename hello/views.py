85c1dec2e6c8171b4335d3d6516edf1a864c6f22
sousben <jeremy.j.bensoussan@gmail.com>
2016-10-05 13:09:35 +0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,20 +10,6 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
-def oauth(request):
-    print 'new app install what else'
-    print request.GET
-    getDict = request.GET
-    print type(getDict)
-    print getDict.get('code','0')
-    # https://slack.com/api/oauth.access?
-
-def poll(request, pollName):
-    print 'user requested poll page with name ' + pollName
-    print type(request)
-    print request.GET
-    return render(request, 'index.html')
-
 def db(request):
     print request
     greeting = Greeting()
